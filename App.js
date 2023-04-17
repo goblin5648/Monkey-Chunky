3ee991e152cf097d84baa779325be72cdd04e9c7
Elliot Brower <you@example.com>
2023-04-17 16:42:37 -0700

    'vbhjytgfvb'

--- App.js
+++ App.js
@@ -98,12 +98,12 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     textAlign: 'center',
-    fontSize: 30,
+    fontSize: 31,
     fontWeight: 'bold',
   },
   imageIcon: {
-    width: 150,
-    height: 150,
-    marginLeft: 95,
+    width: 350,
+    height: 250,
+    marginLeft: 75,
   }
 });
